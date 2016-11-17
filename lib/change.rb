fde3a3481068940b4d79d80d7f3c79cf7fdd8606
Andrew Accuardi and Marty Kovach <andrew@>
2016-11-17 13:25:17 -0800

    Create working specs for quarters, dimes, nickels, pennies, and also include a limit on the amount of quarters given back as change.

--- lib/change.rb
+++ lib/change.rb
@@ -1,25 +1,63 @@
 class Float
   define_method(:disp_change) do |cash|
-    change_get = cash - self
     quarters_get = 0
     dimes_get = 0
+    nickels_get = 0
+    pennies_get = 0
+    remainder = 0
+
+    change_get = cash - self
+
+    if change_get >= 0.25
       disp_q = (change_get / 0.25).to_i
       counter = 0
-      until counter.==(disp_q)
+      until (counter.== (disp_q)) || (counter == 8)
         counter = counter.+(1)
         quarters_get = counter
       end
-      remainder = change_get - (0.25 * quarters_get)
-      if remainder == 0
-        quarters_get.to_s + " quarter(s)"
-      elsif remainder > 0
-        disp_d = (remainder / 0.10).to_i
-        counter = 0
-        until counter == (disp_d)
-          counter = counter + 1
-          dimes_get = counter
-        end
-        quarters_get.to_s + " quarter(s) and " + dimes_get.to_s + " dime(s)"
+      remainder = change_get - (quarters_get * 0.25)
+    elsif change_get < 0.25
+      remainder = change_get
+    end
+
+    if remainder >= 0.10
+    disp_d = (remainder / 0.10).to_i
+    counter = 0
+      until counter == (disp_d)
+        counter = counter + 1
+        dimes_get = counter
       end
+    remainder = remainder - (dimes_get * 0.10)
+    elsif change_get < 0.10
+    remainder = change_get
+    end
+
+    if remainder >= 0.05
+    disp_n = (remainder / 0.05).to_i
+    counter = 0
+      until counter == (disp_n)
+        counter = counter + 1
+        nickels_get = counter
+      end
+    remainder = remainder - (nickels_get * 0.05)
+    elsif change_get < 0.05
+    remainder = change_get
+    end
+
+    if remainder < 0.05
+    disp_p = (remainder / 0.01).to_i
+    counter = 0
+      until counter == (disp_p)
+        counter = counter + 1
+        pennies_get = counter
+      end
+    end
+    message = quarters_get.to_s + " quarter(s) and " + dimes_get.to_s + " dime(s) and " + nickels_get.to_s + " nickel(s) and " + pennies_get.to_s + " pennie(s)"
+
+    if quarters_get == 8
+      "We are now out of quarters, you will recieve the rest of your change in other denominations as available. " + message
+    else
+      message
+    end
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -3,10 +3,21 @@ require("change")
 
 describe("Float#disp_change") do
   it("Will make change in quarter increments") do
-    expect(1.00.disp_change(1.25)).to(eq("1 quarter(s)"))
+    expect(1.00.disp_change(1.25)).to(eq("1 quarter(s) and 0 dime(s) and 0 nickel(s) and 0 pennie(s)"))
   end
 
-  it("will make the remaing change after quarters in dime increments") do
-    expect(1.00.disp_change(1.35)).to(eq("1 quarter(s) and 1 dime(s)"))
+  it("will make the remaining change after quarters in dime increments") do
+    expect(1.00.disp_change(1.35)).to(eq("1 quarter(s) and 1 dime(s) and 0 nickel(s) and 0 pennie(s)"))
+  end
+
+  it("will make the remaining change after quarters, and dimes in nickels") do
+    expect(1.00.disp_change(1.05)).to(eq("0 quarter(s) and 0 dime(s) and 1 nickel(s) and 0 pennie(s)"))
+  end
+
+  it("will make the remaining change after quarters, dimes, and nickels, into pennies") do
+    expect(1.23.disp_change(1.50)).to(eq("1 quarter(s) and 0 dime(s) and 0 nickel(s) and 2 pennie(s)"))
+  end
+  it("Will inhibit the amount of quarters to 8 and make remaining change out of smaller denominations in the least amount of coins possible") do
+    expect(2.44.disp_change(5.00)).to(eq("We are now out of quarters, you will recieve the rest of your change in other denominations as available. 8 quarter(s) and 5 dime(s) and 1 nickel(s) and 1 pennie(s)"))
   end
 end
