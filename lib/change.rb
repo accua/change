483d4353cefa00e9ec95738803c9920d9d38d8a4
Andrew Accuardi and Marty Kovach <andrew@>
2016-11-17 10:26:43 -0800

    Refactor code. Add passing spec for making change with quarters and dimes

--- lib/change.rb
+++ lib/change.rb
@@ -1,14 +1,25 @@
 class Float
   define_method(:disp_change) do |cash|
     change_get = cash - self
-      if (change_get.modulo(0.25) == 0)
-        disp_q = change_get / 0.25
-        counter = 0
-          until counter.==(disp_q)
-          counter = counter.+(1)
-          quarters_get = counter
-          end
+    quarters_get = 0
+    dimes_get = 0
+      disp_q = (change_get / 0.25).to_i
+      counter = 0
+      until counter.==(disp_q)
+        counter = counter.+(1)
+        quarters_get = counter
+      end
+      remainder = change_get - (0.25 * quarters_get)
+      if remainder == 0
         quarters_get.to_s + " quarter(s)"
+      elsif remainder > 0
+        disp_d = (remainder / 0.10).to_i
+        counter = 0
+        until counter == (disp_d)
+          counter = counter + 1
+          dimes_get = counter
+        end
+        quarters_get.to_s + " quarter(s) and " + dimes_get.to_s + " dime(s)"
       end
   end
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -5,4 +5,8 @@ describe("Float#disp_change") do
   it("Will make change in quarter increments") do
     expect(1.00.disp_change(1.25)).to(eq("1 quarter(s)"))
   end
+
+  it("will make the remaing change after quarters in dime increments") do
+    expect(1.00.disp_change(1.35)).to(eq("1 quarter(s) and 1 dime(s)"))
+  end
 end
